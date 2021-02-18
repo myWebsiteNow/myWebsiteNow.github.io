6d1bbdf0afbd67d357cd5b4f37900050e10f6f64
Bennett Lopez <bennettlopez01@gmail.com>
2021-02-18 13:14:40 -0800

    Update to fade

--- landing.js
+++ landing.js
@@ -1,13 +1,20 @@
-// Make Sure that the HTML is fully loaded
-document.addEventListener("DOMContentLoaded", function(event) { 
-    // Stuf to do onload
-});
 
 
-window.addEventListener('scroll', function() {
-    scrollLogic();
-});
 
+function onLoad() {
+    // This sets the inital opacity of the text to 0
+    var titleElement = document.getElementById("BC_Title");
+    var bodyElement = document.getElementById("BC_Body");
+    titleElement.style.opacity = "0.0";
+    bodyElement.style.opacity = "0.0";
+    
+    window.addEventListener('scroll', function() {
+        scrollLogic();
+    });
+    animateCode();
+    iconClick()
+    fadeText(titleElement, bodyElement);
+}
 
 function scrollLogic() {
     var header = document.getElementById('Head_Container');
@@ -23,7 +30,6 @@ function scrollLogic() {
 }
 
 function iconClick() {
-    animateCode();
     var title = document.getElementById("info");
     console.log(title)
     var isClicked = false;
@@ -35,12 +41,10 @@ function iconClick() {
         else {
             if (isClicked) {
                 title.innerHTML = "<p style=\"display:inline-block;color:#F6C639;\" id=\"info\">&nbsp;I</p>";
-                console.log("Click");
                 isClicked = false;
             }
             else {
                 title.innerHTML = "<p style=\"display:inline-block;color:#f5f4f1;\" id=\"info\">&nbsp;I</p>";
-                console.log("Un-Click");
                 isClicked = true;
             }
         }
@@ -76,9 +80,31 @@ function animateCode() {
     }
 }
 
+function fadeText(titleElement, bodyElement) {
+    var opacityTitle = 0.0;
+    var opacitySub = 0.0;
+    
+    
+    var id = setInterval(frame, 30);
+    function frame() {
+        if (opacityTitle > 1 && opacitySub > 1) {
+            clearInterval(id);
+        }
+        else {
+            opacityTitle = opacityTitle + 0.02;
+            opacitySub = opacitySub + 0.02;
+            titleElement.style.opacity = String(opacityTitle)
+            bodyElement.style.opacity = String(opacitySub)
+        }
+    }
+}
+
 
 var codeSnipit = "    # Open the CSV file. This assumes that its comma separated !!   with open(args.path, newline='') as csvFile:!!        csvRows = csv.reader(csvFile, delimiter=',')!!        for currentRow in csvRows:!!            if numElement == 0:!!                pass    # The Header Line!!            else:!!                # Get the different service types in the file!!                if float(currentRow[5]) < minSalary:!!                    minSalary = float(currentRow[5])!!                elif float(currentRow[5]) > maxSalary:!!                    maxSalary = float(currentRow[5]);!!!!                if currentRow[3] not in serviceTypes:!!                    serviceTypes.append(currentRow[3])!!                    serviceTypeCounts.append(0)!!                    serviceAveragePay.append(0)!!                    partTimeAverageOmission.append(0)!!                    partTimeNumberOmission.append(0)!!                    totalSalaries.append([])!!                index = serviceTypes.index(currentRow[3])!!                # Add a count of the number of workers in each service!!                serviceTypeCounts[index] += 1!!                # Add the Average to the category of service!!                serviceAveragePay[index] += float(currentRow[5])!!                totalSalaries[serviceTypes.index(currentRow[3])].append(float(currentRow[5]));!!                if currentRow[4] != \"Part-Time\":!!                    partTimeNumberOmission[index] += 1;!!                   partTimeAverageOmission[index] += float(currentRow[5]);!!!!            numElement += 1!!    # Calculate the average for all workers!!    for i in range(0, len(serviceAveragePay)):!!        serviceAveragePay[i] = round(serviceAveragePay[i] / serviceTypeCounts[i], 2)!!    # Calculate the average for full time workers!!    for i in range(0, len(partTimeAverageOmission)):!!        partTimeAverageOmission[i] = round(partTimeAverageOmission[i] / partTimeNumberOmission[i], 2) "
 
+function circ(timeFraction) {
+  return 1 - Math.sin(Math.acos(timeFraction));
+}
 
 
 
