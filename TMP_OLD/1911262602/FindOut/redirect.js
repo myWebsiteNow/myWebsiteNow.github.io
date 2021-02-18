e32e074df1c62f781e40b5c6a3f5d939f6596b81
Bennett Lopez <bennettlopez01@gmail.com>
2021-02-18 00:30:56 -0800

    ACM-W UPdate v2

---
+++ TMP_OLD/1911262602/FindOut/redirect.js
@@ -0,0 +1,38 @@
+const FILE_PATH = "https://mywebsitenow.github.io/1911262602/FindOut/names.txt"
+var participants = [];
+
+function run() {
+    participants = readTextFile(FILE_PATH).split(/\r\n|\n/);
+}
+
+
+
+function done() {
+    event.preventDefault();
+    var display = "Name not found! Make sure its <u>YOUR</u> first name";
+    var name = document.getElementById("name").value;
+    var i;
+    console.log(participants);
+    for (i = 0; i < participants.length; i++) {
+        var currentLine = participants[i].split(",");
+        if (currentLine[0] == name) {
+            display = "You Got: <u>" + currentLine[1] + "</u> With The Nickname of: <u>" + currentLine[2] + "</u>";
+        }
+    }
+    document.getElementById("topMessage").innerHTML = display;
+}
+
+function readTextFile(file) {
+    var allText = "";
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status === 0) {
+                allText = rawFile.responseText;
+            }
+        }
+    }
+    rawFile.send(null);
+    return allText
+}
---
+++ TMP_OLD/landing.js
@@ -0,0 +1,42 @@
+ const EXTENSION = "https://mywebsitenow.github.io/"
+ 
+ 
+ function formInput() {
+    event.preventDefault();
+    //window.open("http://google.com", "_self");
+    var formInputUser = document.getElementById("inOne").value;
+    var formInputPass = document.getElementById("inTwo").value;
+    
+    var hash = stringToHash(formInputPass);
+    var newDirectory;
+    if (formInputUser == "Steven Asker") {
+        if (hash == 506058779) {
+            newDirectory = stringToHash(formInputPass + "Salty-Salt-Why")
+            console.log("NEW DIRECTORY: " + Math.abs(newDirectory));
+            location.href = EXTENSION + Math.abs(newDirectory) + "/index.html";
+            
+        }
+        else {
+            location.reload();
+        }
+    }
+    else {
+        location.reload();
+    }
+    console.log("USERNAME: " + formInputUser);
+    console.log("PASSWORD: " + formInputPass);
+    console.log("HASH: " + stringToHash(formInputPass));
+ }
+ 
+ 
+ 
+ function stringToHash(string) { 
+    var hash = 0; 
+
+    for (i = 0; i < string.length; i++) { 
+        char = string.charCodeAt(i); 
+        hash = ((hash << 5) - hash) + char; 
+        hash = hash & hash; 
+    } 
+    return hash; 
+} 
